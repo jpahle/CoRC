ed96df4d558558d771430556dc7c6b1abaf6c530
Jonas Förster <J.Foerster@stud.uni-heidelberg.de>
2017-06-14 11:31:45 +0200

    more work on TimeCourse

--- rCopasi/R/task_timecourse.R
+++ rCopasi/R/task_timecourse.R
@@ -14,35 +14,18 @@
 #' @param datamodel a model object
 #' @return a data frame with a time column and species concentration columns
 #' @export
-runTimeCourse <- function(duration, dt, intervals, suppressOutputBefore, outputEvents, saveResultInMemory, startInSteadyState, updateModel, method, datamodel = pkg_env$curr_dm) {
-  assert_that(
-    confirmDatamodel(datamodel),
-    missing(duration)             || is_scalar_numeric(duration)                              && duration >= 0,
-    missing(dt)                   || is_scalar_numeric(dt)                                    && dt >= 0,
-    missing(intervals)            || rlang::is_scalar_integerish(intervals)                   && intervals > 0,
-    missing(suppressOutputBefore) || is_scalar_numeric(suppressOutputBefore)                  && !is.na(suppressOutputBefore),
-    missing(outputEvents)         || is_scalar_logical(outputEvents)                          && !is.na(outputEvents),
-    missing(saveResultInMemory)   || is_scalar_logical(saveResultInMemory)                    && !is.na(saveResultInMemory),
-    missing(startInSteadyState)   || is_scalar_logical(startInSteadyState)                    && !is.na(startInSteadyState),
-    missing(updateModel)          || is_scalar_logical(updateModel)                           && !is.na(updateModel),
-    missing(method)               || (is_scalar_character(method) || is_scalar_list(method))  && !rlang::is_na(method)
-  )
-
-  assert_that(!(!missing(dt) && !missing(intervals)), msg = "Only one of dt and intervals can be given")
+runTimeCourse <- function(duration = NULL, dt = NULL, intervals = NULL, suppressOutputBefore = NULL, outputEvents = NULL, saveResultInMemory = NULL, startInSteadyState = NULL, updateModel = NULL, method = NULL, datamodel = pkg_env$curr_dm) {
+  assert_that(confirmDatamodel(datamodel))
+  
+  confirmTimeCourseSettings()
+  confirmTimeCourseMethod()
 
   task <- as(datamodel$getTask("Time-Course"), "_p_CTrajectoryTask")
-  assert_that(!is_null(task))
-  # if (is.null(task)) {
-  #   # create a new one
-  #   task <- CSteadyStateTask()
-  #   # add the new task to the task list
-  #   dataModel$getTaskList()$addAndOwn(task)
-  # }
 
   problem <- as(task$getProblem(), "_p_CTrajectoryProblem")
 
   restoreCall <-
-    setupTimeCourse(
+    setTimeCourseSettings(
       problem = problem,
       duration = duration,
       dt = dt,
@@ -53,11 +36,11 @@ runTimeCourse <- function(duration, dt, intervals, suppressOutputBefore, outputE
       startInSteadyState = startInSteadyState,
       updateModel = updateModel
     )
-
-  setupTimeCourseMethod(task = task, method = method)
-
-  # Not sure if this is needed
-  # problem$setModel(datamodel$getModel())
+  
+  setTimeCourseMethod(
+    task = task,
+    method = method
+  )
 
   # datamodel$getModel()$setInitialTime(initialtime)
 
@@ -85,63 +68,121 @@ runTimeCourse <- function(duration, dt, intervals, suppressOutputBefore, outputE
     class(ret) <- prepend(class(ret), "copasi_ts")
   } else {
     ret <- NULL
-    if (missing(saveResultInMemory)) warning("No results generated because saveResultInMemory is set to FALSE in the model")
+    if (is.null(saveResultInMemory)) warning("No results generated because saveResultInMemory is set to FALSE in the model. Explicitly set the argument to silence this warning.")
   }
 
-  do.call(setupTimeCourse, restoreCall)
+  do.call(setTimeCourseSettings, restoreCall)
 
   ret
 }
 
-setupTimeCourse <- function(problem, duration, dt, intervals, suppressOutputBefore, outputEvents, saveResultInMemory, startInSteadyState, updateModel) {
+setupTimeCourse <- function(duration = NULL, dt = NULL, intervals = NULL, suppressOutputBefore = NULL, outputEvents = NULL, saveResultInMemory = NULL, startInSteadyState = NULL, updateModel = NULL, method = NULL, datamodel = pkg_env$curr_dm) {
+  assert_that(confirmDatamodel(datamodel))
+  
+  confirmTimeCourseSettings()
+  confirmTimeCourseMethod()
+  
+  task <- as(datamodel$getTask("Time-Course"), "_p_CTrajectoryTask")
+  
+  problem <- as(task$getProblem(), "_p_CTrajectoryProblem")
+  
+  setTimeCourseSettings(
+    problem = problem,
+    duration = duration,
+    dt = dt,
+    intervals = intervals,
+    suppressOutputBefore = suppressOutputBefore,
+    outputEvents = outputEvents,
+    saveResultInMemory = saveResultInMemory,
+    startInSteadyState = startInSteadyState,
+    updateModel = updateModel
+  )
+  
+  setTimeCourseMethod(
+    task = task,
+    method = method
+  )
+  
+  invisible()
+}
+
+confirmTimeCourseSettings <- function() {
+  evalq(
+    envir = 1,
+    {
+      assert_that(
+        is.null(duration)             || is_scalar_numeric(duration)                              && duration >= 0,
+        is.null(dt)                   || is_scalar_numeric(dt)                                    && dt >= 0,
+        is.null(intervals)            || rlang::is_scalar_integerish(intervals)                   && intervals > 0,
+        is.null(suppressOutputBefore) || is_scalar_numeric(suppressOutputBefore)                  && !is.na(suppressOutputBefore),
+        is.null(outputEvents)         || is_scalar_logical(outputEvents)                          && !is.na(outputEvents),
+        is.null(saveResultInMemory)   || is_scalar_logical(saveResultInMemory)                    && !is.na(saveResultInMemory),
+        is.null(startInSteadyState)   || is_scalar_logical(startInSteadyState)                    && !is.na(startInSteadyState),
+        is.null(updateModel)          || is_scalar_logical(updateModel)                           && !is.na(updateModel)
+      )
+      assert_that(!(!is.null(dt) && !is.null(intervals)), msg = "Only one of dt and intervals can be given")
+    }
+  )
+}
+
+setTimeCourseSettings <- function(problem, duration = NULL, dt = NULL, intervals = NULL, suppressOutputBefore = NULL, outputEvents = NULL, saveResultInMemory = NULL, startInSteadyState = NULL, updateModel = NULL) {
   restoreCall <- list(problem = problem)
 
-  if (!missing(duration)) {
+  if (!is.null(duration)) {
     restoreCall$duration <- problem$getDuration()
     problem$setDuration(duration)
   }
 
-  if (!missing(dt)) {
+  if (!is.null(dt)) {
     restoreCall$dt <- problem$getStepSize()
     problem$setStepSize(dt)
   }
 
-  if (!missing(intervals)) {
+  if (!is.null(intervals)) {
     restoreCall$intervals <- problem$getStepNumber()
     problem$setStepNumber(intervals)
   }
 
-  if (!missing(suppressOutputBefore)) {
+  if (!is.null(suppressOutputBefore)) {
     restoreCall$suppressOutputBefore <- problem$getOutputStartTime()
     problem$setOutputStartTime(suppressOutputBefore)
   }
 
-  if (!missing(outputEvents)) {
+  if (!is.null(outputEvents)) {
     restoreCall$outputEvents <- problem$getOutputEvent()
     problem$setOutputEvent(outputEvents)
   }
 
-  if (!missing(saveResultInMemory)) {
+  if (!is.null(saveResultInMemory)) {
     restoreCall$saveResultInMemory <- problem$timeSeriesRequested()
     problem$setTimeSeriesRequested(saveResultInMemory)
   }
 
-  if (!missing(startInSteadyState)) {
+  if (!is.null(startInSteadyState)) {
     restoreCall$startInSteadyState <- problem$getStartInSteadyState()
     problem$setStartInSteadyState(startInSteadyState)
   }
 
-  if (!missing(updateModel)) stop("updateModel not yet implemented")
+  if (!is.null(updateModel)) stop("updateModel not yet implemented")
 
   restoreCall
 }
 
-setupTimeCourseMethod <- function(task, method) {
+confirmTimeCourseMethod <- function() {
+  evalq(
+    envir = 1,
+    {
+      assert_that(is.null(method) || (is_scalar_character(method) || is_scalar_list(method)) && !rlang::is_na(method))
+    }
+  )
+}
+
+setTimeCourseMethod <- function(task, method = NULL) {
   validMethods <- names(.__E___CTaskEnum__Method)[task$getValidMethods()]
 
-  if (!missing(method)) {
+  if (!is.null(method)) {
     if (is_scalar_character(method)) {
-      task$setMethodType(match.arg(method, validMethods))
+      task$setMethodType(rlang::arg_match(method, validMethods))
     } else {
       stop("Detailed method definition not yet implemented.")
     }
