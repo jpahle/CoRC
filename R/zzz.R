e5b7942f971cdfed83e94a42456072c26383e164
Jonas Förster <J.Foerster@stud.uni-heidelberg.de>
2017-06-22 16:35:36 +0200

    fixed package for windows

--- R/zzz.R
+++ R/zzz.R
@@ -1,28 +1,27 @@
 .onLoad <- function(libname, pkgname) {
-  shared_lib_path <- file.path(system.file("libs", package = pkgname), paste0("COPASI", .Platform$dynlib.ext))
-
-  # hack for load_all()
+  # hack for load_all() 
   libname <- .libPaths()
   
-  if (file.exists(shared_lib_path)) library.dynam("COPASI", pkgname, libname)
+  iserr <- try(library.dynam("COPASI", pkgname, libname), silent = TRUE)
+  if (is(iserr, "try-error")) warning(pkgname, ": Copasi binaries are not installed. Use getCopasi to install them.")
 }
 
 .onUnload <- function(libpath) {
-  if ("COPASI" %in% map_chr(.dynLibs(), "name")) library.dynam.unload("COPASI", libpath)
+  try(library.dynam.unload("COPASI", libpath), silent = TRUE)
 }
 
 #' @export
 getCopasi <- function(path) {
-  libsdir <- file.path(system.file(package = "CoRC"), "libs")
+  libsdir <- file.path(system.file(package = getPackageName()), "libs", "x64")
   success <- TRUE
-  if (!dir.exists(libsdir)) success <- dir.create(libsdir)
+  if (!dir.exists(libsdir)) success <- dir.create(libsdir, recursive = TRUE)
   libfile <- file.path(libsdir, paste0("COPASI", .Platform$dynlib.ext))
   if (success) success <- file.copy(path, libfile)
 
   if (success) {
-    library.dynam("COPASI", "CoRC", .libPaths())
-    cat("CoRC: Successfully loaded copasi binaries.")
+    library.dynam("COPASI", getPackageName(), .libPaths())
+    message(getPackageName(), ": Successfully loaded copasi binaries.")
   } else {
-    cat("CoRC: Loading of copasi binaries failed.")
+    message(getPackageName(), ": Loading of copasi binaries failed.")
   }
 }
