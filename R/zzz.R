180bcc02b0bf2f716c53b5f73defe6e69c79b5b7
Jonas Förster <J.Foerster@stud.uni-heidelberg.de>
2017-06-22 12:57:23 +0200

    attempt to fix behavior with devtools

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,10 @@
 .onLoad <- function(libname, pkgname) {
   shared_lib_path <- file.path(system.file("libs", package = pkgname), paste0("COPASI", .Platform$dynlib.ext))
+
+  # hack for load_all()
+  libname <- .libPaths()
   
   if (file.exists(shared_lib_path)) library.dynam("COPASI", pkgname, libname)
-
-  invisible()
 }
 
 .onUnload <- function(libpath) {
