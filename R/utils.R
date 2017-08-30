1bd85324f4bf27d322ae58bde7419c4afaac7fdb
Jonas Förster <J.Foerster@stud.uni-heidelberg.de>
2017-08-30 14:26:07 +0200

    - some work on getting/setting method parameters

--- R/utils.R
+++ R/utils.R
@@ -59,8 +59,12 @@ assert_datamodel <- function(c_datamodel) {
 transform_names <- function(x) {
   set_names(
     x,
-    names(x) %>% make.names(unique = TRUE) %>% stringr::str_to_lower()
+    transform_names_worker(names(x))
   )
+}
+
+transform_names_worker <- function(x) {
+  x %>% make.names(unique = TRUE) %>% stringr::str_to_lower()
 }
 
 # finds copasi messages and helps purge known annoying messages
@@ -166,16 +170,16 @@ autoplot.copasi_ts <- function(object, ...) {
 # It is our responsibility to call the right function which is why this list is needed.
 cparameter_control_functions <-
   list(
-    DOUBLE = is_scalar_numeric,
-    UDOUBLE = (function(x) {is_scalar_numeric(x) && x >= 0}),
+    DOUBLE = function(x) {is_scalar_integer(x) || is_scalar_double(x)},
+    UDOUBLE = function(x) {(is_scalar_integer(x) || is_scalar_double(x)) && x >= 0},
     INT = rlang::is_scalar_integerish,
-    UINT = (function(x) {rlang::is_scalar_integerish(x) && x >= 0}),
-    BOOL = (function(x) {length(x) != 0L && (x == 1L || x == 0L)}),
+    UINT = function(x) {rlang::is_scalar_integerish(x) && x >= 0},
+    BOOL = function(x) {length(x) != 0L && (x == 1L || x == 0L)},
     STRING = is_scalar_character,
     GROUP = NULL,
-    CN = NULL,
-    KEY = NULL,
-    FILE = NULL,
+    CN = is_scalar_character,
+    KEY = is_scalar_character,
+    FILE = is_scalar_character,
     EXPRESSION = NULL,
     INVALID = NULL
   )
@@ -188,12 +192,15 @@ cparameter_get_functions <-
     UINT = CCopasiParameter_getUIntValue,
     BOOL = CCopasiParameter_getBoolValue,
     STRING = CCopasiParameter_getStringValue,
-    GROUP = CCopasiParameter_setGroupValue,
-    CN = CCopasiParameter_setCNValue,
-    KEY = CCopasiParameter_setKeyValue,
-    FILE = CCopasiParameter_setFileValue,
-    EXPRESSION = NULL,
-    INVALID = NULL
+    # GROUP = CCopasiParameter_getGroupValue,
+    GROUP = function(self) {"UNSUPPORTED_PARAMETER"},
+    CN = function(self) {
+      CCopasiParameter_getCNValue(self)$getString()
+    },
+    KEY = CCopasiParameter_getKeyValue,
+    FILE = CCopasiParameter_getFileValue,
+    EXPRESSION = function(self) {"UNSUPPORTED_PARAMETER"},
+    INVALID = function(self) {"UNSUPPORTED_PARAMETER"}
   )
 
 cparameter_set_functions <-
@@ -205,7 +212,9 @@ cparameter_set_functions <-
     BOOL = CCopasiParameter_setBoolValue,
     STRING = CCopasiParameter_setStringValue,
     GROUP = CCopasiParameter_setGroupValue,
-    CN = CCopasiParameter_setCNValue,
+    CN = function(self, x) {
+      CCopasiParameter_setCNValue(self, CCommonName(x))
+    },
     KEY = CCopasiParameter_setKeyValue,
     FILE = CCopasiParameter_setFileValue,
     EXPRESSION = NULL,
@@ -216,16 +225,83 @@ methodstructure <- function(c_method) {
   struct <-
     tibble::tibble(
       object = seq_along_v(c_method) %>% map(~ c_method$getParameter(.x)),
-      name = object %>% map_chr(~ .x$getObjectName()) %>% make.names(unique = TRUE),
+      name = object %>% map_chr(~ .x$getObjectName()) %>% transform_names_worker(),
       type = object %>% map_chr(~ .x$getType()),
       control_fun = cparameter_control_functions[type],
       get_fun = cparameter_get_functions[type],
       set_fun = cparameter_set_functions[type]
     )
   
-  if (has_element(struct$control_fun, NULL)) warning("Unknown type found with parameters: ", paste(struct$name[map_lgl(struct$control_fun, is_null)], collapse = "; "))
+  if (has_element(struct$control_fun, NULL)) warning("Internal: Unknown type found with parameters: ", paste(struct$name[map_lgl(struct$control_fun, is_null)], collapse = "; "))
   
   struct
+}
+
+get_method_settings <- function(c_method, with_name = FALSE) {
+  struct <- methodstructure(c_method)
+  ret <- list()
+  
+  if (with_name) ret$method <- c_method$getSubType()
+  
+  params <- map2(struct$get_fun, struct$object, ~ .x(.y))
+  names(params) <- struct$name
+  
+  c(ret, params)
+}
+
+set_method_settings <- function(c_method, value_l) {
+  struct <- methodstructure(c_method) %>% tibble::rowid_to_column()
+  
+  data <-
+    tibble::tibble(value = value_l) %>%
+    dplyr::mutate(rowid = pmatch(names(value_l), struct$name))
+  
+  matched <- !is.na(data$rowid)
+  
+  assert_that(
+    all(matched),
+    msg = paste0(
+      "Bad method parameter names.\n",
+      '"', names(value_l)[!matched][1], '" should be one of "',
+      paste0(struct$name, collapse = '", "'), '".'
+    )
+  )
+  
+  data <-
+    data %>%
+    dplyr::filter(map_lgl(value, negate(is_null))) %>%
+    dplyr::left_join(struct, by = "rowid")
+  
+  skipped <- map_lgl(data$control_fun, is_null)
+  
+  skipped %>%
+    which() %>%
+    walk(~
+      warning('Parameter "', data$name[.x], '" was skipped because it is of unsupported type "', data$struct[.x], '".')
+    )
+  
+  data <- data %>% filter(!skipped)
+  
+  allowed <- map2_lgl(data$control_fun, data$value, ~ .x(.y))
+  
+  assert_that(
+    all(allowed),
+    msg = paste0(
+      'Method parameter "', data$name[!allowed][1], '" has to be of type "', data$type[!allowed][1], '".'
+    )
+  )
+  
+  success <- pmap_lgl(
+    data,
+    function(set_fun, object, value, ...) {
+      set_fun(object, value)
+    }
+  )
+  
+  assert_that(
+    all(success),
+    msg = paste0('Method parameter "', data$name[!success][1], '" could not be set.')
+  )
 }
 
 # Convert annotated matrices to data frames
