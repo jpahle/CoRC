a52d9fd261e477141d2adc026c8bfd26df5d9cfa
Jonas Förster <J.Foerster@stud.uni-heidelberg.de>
2017-09-27 11:47:48 +0200

    - fix tests

--- tests/testthat/test_quantity_accessors.R
+++ tests/testthat/test_quantity_accessors.R
@@ -1,17 +1,19 @@
 context("Quantity accessors")
 
-models <- loadExamples()
+loadExamples(1)
 
 test_that("getSpecies gives expected results", {
-  species <- getSpecies(models$brusselator)
-  expect_equal_to_reference(species, "brusselator_species.rds")
-  species <- getSpecies(models$chemotaxis)
-  expect_equal_to_reference(species, "chemotaxis_species.rds")
+  species <- getSpecies()
+  expect_length(species, 12L)
+  expect_true(nrow(species) == 6L)
+  species_b <- getSpecies("B")
+  expect_equal(species_b$initial_concentration, 3)
+  expect_identical(species_b$concentration, NaN)
 })
 
-test_that("setSpecies functions", {
-  expect_silent(setSpecies(getSpecies(models$brusselator)))
-  expect_silent(setSpecies(getSpecies(models$chemotaxis)))
+test_that("setSpecies works", {
+  setSpecies("B", initial_concentration = 5.5)
+  expect_equal(getSpecies("B")$initial_concentration, 5.5)
 })
 
 unloadAllModels()
