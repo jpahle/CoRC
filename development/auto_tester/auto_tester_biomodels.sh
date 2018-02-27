dd0a40a9b30ba8aed658b6bd2ad438d4cbd2460e
Jonas Förster <jonasfoerster@gmail.com>
2018-02-27 14:00:04 +0100

    - rename the biomodels test file

--- development/auto_tester/auto_tester_biomodels.sh
+++ development/auto_tester/auto_tester_biomodels.sh
@@ -4,4 +4,4 @@ set -x
 
 docker build -t corc_auto_tester test_container 
 
-docker run corc_auto_tester R -e 'source(system.file("tests", "other", "biomodels_test.R", package = "CoRC"))'
+docker run corc_auto_tester R -e 'source(system.file("tests", "other", "test_biomodels.R", package = "CoRC"))'
