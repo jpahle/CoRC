5c622d73a21dd2f4789efc28809837c3ba861f34
Jonas Förster <jonasfoerster@gmail.com>
2018-07-31 22:24:07 +0200

    - clarify site_upgrade.R

--- development/site_upgrade.R
+++ development/site_upgrade.R
@@ -1,12 +1,15 @@
 # For upgrading the site I want to have the latest versions of all relevant packages
 
+# reinstall magick to keep up with system imagemagick
 try(remove.packages("magick"))
 install.packages("magick")
+# use latest roxygen out of necessity until a version > 6.0.1 is official for some necessary features
 devtools::install_github("klutometis/roxygen")
+# use latest pkgdown for latest features for now
 devtools::install_github("r-lib/pkgdown")
-devtools::install_github("tidyverse/ggplot2")
-# for now, use fixed plotly version, as, more recent, fail to scale ggplotly() properly
-devtools::install_github("ropensci/plotly", ref = "d7eaf38e915462db75ae09790fc1b7067e10091a")
+# use fixed plotly versions to be able to keep the 8mb or so of plots frozen when updating the rest of the website
+remotes::install_version("ggplot2", "3.0.0")
+remotes::install_version("plotly", "4.8.0")
 devtools::document()
 devtools::install()
 devtools::test()
