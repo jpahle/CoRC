20a595a8932e92d1a98ba3539d446b05ad003746
Jonas Förster <jonasfoerster@gmail.com>
2020-05-22 18:44:55 +0200

    Upgrade website dependencies

--- dev/site_upgrade.R
+++ dev/site_upgrade.R
@@ -1,10 +1,10 @@
 # For upgrading the site I want to have specific versions of relevant packages
 
 # use specific pkgdown to keep page stable for now
-remotes::install_version("pkgdown", "1.4.1", upgrade = "never")
+remotes::install_version("pkgdown", "1.5.1", upgrade = "never")
 # use fixed plotly versions to be able to keep the 8mb or so of plots frozen when updating the rest of the website
-remotes::install_version("ggplot2", "3.2.1", upgrade = "never")
-remotes::install_version("plotly", "4.9.0", upgrade = "never")
+remotes::install_version("ggplot2", "3.3.0", upgrade = "never")
+remotes::install_version("plotly", "4.9.2.1", upgrade = "never")
 devtools::document()
 # the parallel cluster used in the examples vignette needs to have CoRC installed
 devtools::install(upgrade = "never")
