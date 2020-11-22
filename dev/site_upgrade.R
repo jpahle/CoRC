c3ca38e9519d5ddbef1e17342ab6c108ca0dd483
Jonas Förster <jonasfoerster@gmail.com>
2020-11-23 00:37:40 +0100

    Update pkgdown

--- dev/site_upgrade.R
+++ dev/site_upgrade.R
@@ -1,7 +1,7 @@
 # For upgrading the site I want to have specific versions of relevant packages
 
 # use specific pkgdown to keep page stable for now
-remotes::install_version("pkgdown", "1.5.1", upgrade = "never")
+remotes::install_version("pkgdown", "1.6.1", upgrade = "never")
 # use fixed plotly versions to be able to keep the 8mb or so of plots frozen when updating the rest of the website
 remotes::install_version("ggplot2", "3.3.0", upgrade = "never")
 remotes::install_version("plotly", "4.9.2.1", upgrade = "never")
