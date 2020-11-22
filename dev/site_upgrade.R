906d0c9a40bcfd2c739ba8501ac09394df8c1200
Jonas Förster <jonasfoerster@gmail.com>
2020-11-23 00:37:40 +0100

    Upgrade Website

--- dev/site_upgrade.R
+++ dev/site_upgrade.R
@@ -3,7 +3,7 @@
 # use specific pkgdown to keep page stable for now
 remotes::install_version("pkgdown", "1.6.1", upgrade = "never")
 # use fixed plotly versions to be able to keep the 8mb or so of plots frozen when updating the rest of the website
-remotes::install_version("ggplot2", "3.3.0", upgrade = "never")
+remotes::install_version("ggplot2", "3.3.2", upgrade = "never")
 remotes::install_version("plotly", "4.9.2.1", upgrade = "never")
 devtools::document()
 # the parallel cluster used in the examples vignette needs to have CoRC installed
